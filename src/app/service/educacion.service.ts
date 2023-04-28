4894d3f5586432d7b0bdf71b3d7c38bcd4fdb59d
Milicatlu <luchianavillarroel8@gmail.com>
2023-04-28 02:03:32 -0300

    se

--- src/app/service/educacion.service.ts
+++ src/app/service/educacion.service.ts
@@ -7,7 +7,7 @@ import { Educacion } from '../model/educacion';
   providedIn: 'root',
 })
 export class EducacionService {
-  URL = 'http://localhost:8080/educacion/';
+  URL = 'https://backendlv.onrender.com';
   constructor(private httclient: HttpClient) {}
 
   public lista(): Observable<Educacion[]> {
--- src/app/service/skill.service.ts
+++ src/app/service/skill.service.ts
@@ -7,7 +7,7 @@ import { Skill } from '../model/skill';
   providedIn: 'root'
 })
 export class SkillService {
-  URL= 'http://localhost:8080/skill/'
+  URL= 'https://backendlv.onrender.com'
   constructor(private HttpClient: HttpClient) { }
 
   public lista(): Observable<Skill[]>{
